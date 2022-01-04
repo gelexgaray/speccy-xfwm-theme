3a33de26697995dbd11b3ba765d7cf73a941f782
Gorka Elexgaray <gelexgaray@yahoo.es>
2022-01-04 19:27:14 +0000

    Initial commit

---
+++ xfwm4/copy.sh
@@ -0,0 +1,24 @@
+# this script is for easying theme development :P
+
+#alareunat
+cp bottom-active.xpm bottom-inactive.xpm
+cp left-active.xpm left-inactive.xpm
+cp right-active.xpm right-inactive.xpm
+
+#kulmat
+cp bottom-left-active.xpm bottom-left-inactive.xpm
+cp bottom-right-active.xpm bottom-right-inactive.xpm
+cp top-right-active.xpm top-right-inactive.xpm
+cp top-left-active.xpm top-left-inactive.xpm
+
+#yläreuna
+cp title-1-active.xpm title-1-inactive.xpm
+cp title-1-active.xpm title-2-active.xpm
+cp title-1-active.xpm title-2-inactive.xpm
+cp title-1-active.xpm title-3-active.xpm
+cp title-1-active.xpm title-3-inactive.xpm
+cp title-1-active.xpm title-4-active.xpm
+cp title-1-active.xpm title-4-inactive.xpm
+cp title-1-active.xpm title-5-active.xpm
+cp title-1-active.xpm title-5-inactive.xpm
+
---
+++ xfwm4/readme.md
@@ -0,0 +1,2 @@
+Based on prelude-dfr from DEFER
+https://www.xfce-look.org/p/1016457
